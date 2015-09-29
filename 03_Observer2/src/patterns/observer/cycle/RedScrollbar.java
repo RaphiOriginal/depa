ff889cc35f9e7194867f1fe27ce68cd61ad4cb37
Raphael Brunner <raphi.brunner@me.com>
2015-09-29 10:16:38 +0200

    added a example project to see and clear some observer problems

---
+++ 03_Observer2/src/patterns/observer/cycle/RedScrollbar.java
@@ -0,0 +1,31 @@
+package patterns.observer.cycle;
+
+import java.awt.Color;
+
+public class RedScrollbar extends Observable implements Observer {
+	
+	private int value;
+	
+	public RedScrollbar(ColorModel model) {
+		this.value = model.getColor().getRed();
+		model.addObserver(this);
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		System.out.println("RedScrollbar.setValue");
+		this.value = value;
+		this.notifyObservers(value);
+	}
+
+	@Override
+	public void update(Observable source, Object arg) {
+		System.out.println("model changed its color => adjust scrollbar value");
+		int color = ((Color)arg).getRed();
+		if(color != value) setValue(color);
+	}
+
+}
