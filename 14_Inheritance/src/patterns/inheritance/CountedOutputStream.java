b7c3e80022432b4599214fa265d3671411bf66c7
Raphael Brunner <raphi.brunner@me.com>
2016-01-12 10:00:53 +0100

    fix for inheritance problem

--- 14_Inheritance/src/patterns/inheritance/CountedOutputStream.java
+++ 14_Inheritance/src/patterns/inheritance/CountedOutputStream.java
@@ -3,6 +3,9 @@ package patterns.inheritance;
 import java.io.*;
 
 public class CountedOutputStream extends StringOutputStream {
+	
+	private int c;
+	private int depth = 0;
 
 	public CountedOutputStream(OutputStream s) {
 		super(s);
@@ -10,19 +13,25 @@ public class CountedOutputStream extends StringOutputStream {
 
 	public int writtenChars() {
 		// TODO return number of written characters
-		return 0;
+		return c;
 	}
 
 	@Override
 	public void write(char ch) throws IOException {
 		// TODO Auto-generated method stub
+		if(depth == 0) c++;
+		depth++;
 		super.write(ch);
+		depth--;
 	}
 
 	@Override
 	public void write(String s) throws IOException {
 		// TODO Auto-generated method stub
+		if(depth == 0) c += s.length();
+		depth++;
 		super.write(s);
+		depth--;
 	}
 
 }
